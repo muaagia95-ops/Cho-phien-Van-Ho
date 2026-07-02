44da52fb4b2dbf2a06f3480dc43b98cb0eb65aaa
muaagia95-ops <muaagia95@gmail.com>
2026-07-02 15:02:11 +0700

    nang cap gian hang

---
+++ gian-hang-data.js
@@ -0,0 +1,343 @@
+const SHOP_DATA = {
+  "imageMax": 100,
+  "shops": [
+    {
+      "id": "01",
+      "name": "Gian hàng 01",
+      "group": "Nông sản",
+      "shortDesc": "Rau củ, quả tươi, sản phẩm địa phương.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Rau củ, quả tươi, sản phẩm địa phương."
+      ],
+      "folder": "images/gian-hang/01"
+    },
+    {
+      "id": "02",
+      "name": "Gian hàng 02",
+      "group": "OCOP",
+      "shortDesc": "Sản phẩm đặc trưng, quà tặng địa phương.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Sản phẩm đặc trưng, quà tặng địa phương."
+      ],
+      "folder": "images/gian-hang/02"
+    },
+    {
+      "id": "03",
+      "name": "Gian hàng 03",
+      "group": "Ẩm thực",
+      "shortDesc": "Các món ăn dân tộc, món ăn truyền thống.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Các món ăn dân tộc, món ăn truyền thống."
+      ],
+      "folder": "images/gian-hang/03"
+    },
+    {
+      "id": "04",
+      "name": "Gian hàng 04",
+      "group": "Thủ công",
+      "shortDesc": "Thổ cẩm, sản phẩm lưu niệm, đồ thủ công.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Thổ cẩm, sản phẩm lưu niệm, đồ thủ công."
+      ],
+      "folder": "images/gian-hang/04"
+    },
+    {
+      "id": "05",
+      "name": "Gian hàng 05",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/05"
+    },
+    {
+      "id": "06",
+      "name": "Gian hàng 06",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/06"
+    },
+    {
+      "id": "07",
+      "name": "Gian hàng 07",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/07"
+    },
+    {
+      "id": "08",
+      "name": "Gian hàng 08",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/08"
+    },
+    {
+      "id": "09",
+      "name": "Gian hàng 09",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/09"
+    },
+    {
+      "id": "10",
+      "name": "Gian hàng 10",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/10"
+    },
+    {
+      "id": "11",
+      "name": "Gian hàng 11",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/11"
+    },
+    {
+      "id": "12",
+      "name": "Gian hàng 12",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/12"
+    },
+    {
+      "id": "13",
+      "name": "Gian hàng 13",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/13"
+    },
+    {
+      "id": "14",
+      "name": "Gian hàng 14",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/14"
+    },
+    {
+      "id": "15",
+      "name": "Gian hàng 15",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/15"
+    },
+    {
+      "id": "16",
+      "name": "Gian hàng 16",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/16"
+    },
+    {
+      "id": "17",
+      "name": "Gian hàng 17",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/17"
+    },
+    {
+      "id": "18",
+      "name": "Gian hàng 18",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/18"
+    },
+    {
+      "id": "19",
+      "name": "Gian hàng 19",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/19"
+    },
+    {
+      "id": "20",
+      "name": "Gian hàng 20",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/20"
+    },
+    {
+      "id": "21",
+      "name": "Gian hàng 21",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/21"
+    },
+    {
+      "id": "22",
+      "name": "Gian hàng 22",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/22"
+    },
+    {
+      "id": "23",
+      "name": "Gian hàng 23",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/23"
+    },
+    {
+      "id": "24",
+      "name": "Gian hàng 24",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/24"
+    },
+    {
+      "id": "25",
+      "name": "Gian hàng 25",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/25"
+    },
+    {
+      "id": "26",
+      "name": "Gian hàng 26",
+      "group": "Chưa cập nhật",
+      "shortDesc": "Thông tin gian hàng đang được cập nhật.",
+      "owner": "Chưa cập nhật",
+      "phone": "Chưa cập nhật",
+      "address": "Chợ phiên Vân Hồ, xã Vân Hồ, tỉnh Sơn La",
+      "products": [
+        "Chưa cập nhật"
+      ],
+      "folder": "images/gian-hang/26"
+    }
+  ]
+};
